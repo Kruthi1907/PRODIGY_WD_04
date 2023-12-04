0bacfa1e311d14f73e1ae85da47b8e980e6aec0a
kruthi <sreekruthikaveri@gmail.com>
2023-12-04 15:43:09 +0530

    Tak-5 Done

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# PRODIGY_WD_05
+# WeatherHere
+
+WeatherHere is a simple web application that allows users to get the current weather information for a specified location.
+
+## Features
+
+- **Weather Information:** Retrieve real-time weather data including temperature, weather description, and humidity.
+- **Responsive Design:** The application is designed to be responsive, providing a good user experience on various devices.
+
+## Getting Started
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/WeatherHere.git
+   ```
+
+2. Open the `index.html` file in a web browser or deploy the project on a web server.
+
+3. Enter a location in the input field and click the "Get Weather" button to fetch weather data.
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+
+## API Usage
+
+Weather data is retrieved using the [OpenWeatherMap API](https://openweathermap.org/api). You will need to obtain an API key and replace the placeholder in the `script.js` file with your own key.
+
+```javascript
+const apiKey = 'your-api-key';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+```
---
+++ script.js
@@ -0,0 +1,32 @@
+function getWeather() {
+    const locationInput = document.getElementById('location');
+    const location = locationInput.value;
+
+    const apiKey = 'ef6d52ea4e93c967ef4ad5c6c4844a94';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            alert('Error fetching weather data. Please try again.');
+        });
+}
+
+function displayWeather(data) {
+    const weatherContainer = document.getElementById('weather-info');
+
+    // Create elements for weather information
+    const weatherInfo = document.createElement('div');
+    weatherInfo.innerHTML = `
+        <h2>Current Weather in ${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+    `;
+    weatherContainer.innerHTML = '';
+    weatherContainer.appendChild(weatherInfo);
+}
